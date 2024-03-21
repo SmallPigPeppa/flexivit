6915437786fb69e6a40b57f9dd51c3996db50b1e
lwz <905328134@qq.com>
2024-03-21 22:52:52 +0800

    initial

--- eval_new_new.py
+++ eval_new_new.py
@@ -52,7 +52,7 @@ class ClassificationEvaluator(pl.LightningModule):
                                     checkpoint_path=self.ckpt_path)
             self.net = create_model(self.weights, pretrained=False)
             model_path = './vit_base_patch16_224.augreg_in21k_ft_in1k.pth'
-            model.load_state_dict(torch.load(model_path))
+            self.net.load_state_dict(torch.load(model_path))
             state_dict = self.net.state_dict()
         else:
             orig_net = create_model(self.weights, pretrained=True)
