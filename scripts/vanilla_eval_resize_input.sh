c43cd4dc9448a688163f396ce247a64febecb054
lwz <905328134@qq.com>
2024-03-23 02:17:46 +0800

    initial

--- scripts/vanilla_eval_resize_input.sh
+++ scripts/vanilla_eval_resize_input.sh
@@ -21,13 +21,13 @@ python vanilla_eval_resize_input.py \
   --devices 8 \
   --precision 16 \
   --model.resize_type pi \
-  --model.weights vit_base_patch16_224.augreg_in21k_ft_in1k \
+  --model.weights vit_base_patch16_224_miil.in21k_ft_in1k \
   --data.root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --data.num_classes 1000 \
   --model.patch_size 16 \
   --data.size 224 \
   --data.crop_pct 0.9 \
   --data.batch_size 256 \
-  --model.results_path ./vit_base_patch16_224.augreg_in21k_ft_in1k.csv
+  --model.results_path ./vit_base_patch16_224_miil.in21k_ft_in1k.csv
 
 
