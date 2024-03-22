752bcfe8372c0e85379f3f3af719b644f24f04a3
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2024-03-22 10:13:15 +0800

    initial

--- data_utils/imagenet_val.py
+++ data_utils/imagenet_val.py
@@ -72,7 +72,7 @@ class DataModule(pl.LightningDataModule):
             transforms.Resize(256, interpolation=InterpolationMode.BICUBIC),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.228, 0.224, 0.225]),
         ])
 
     def setup(self, stage="test"):
