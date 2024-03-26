9137fb727da661a1953a00f92c9af13981757f74
lwz <905328134@qq.com>
2024-03-26 13:05:35 +0800

    initial

--- vanilla_eval_resize_input2.py
+++ vanilla_eval_resize_input2.py
@@ -103,8 +103,10 @@ class ClassificationEvaluator(pl.LightningModule):
         acc = self.acc(pred, y)
 
         # Log
-        self.log(f"test_loss", loss, sync_dist=True,on_epoch=True)
-        self.log(f"test_acc", acc, sync_dist=True,on_epoch=True)
+        # self.log(f"test_loss", loss, sync_dist=True,on_epoch=True)
+        # self.log(f"test_acc", acc, sync_dist=True,on_epoch=True)
+        # log the outputs!
+        self.log_dict({'test_loss': loss, 'test_acc': acc})
         # self.log(f"test_loss", loss)
         # self.log(f"test_acc", acc)
 
