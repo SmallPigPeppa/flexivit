47505f02129d6aa8e03bcd57d3acca7d445f7fe0
lwz <905328134@qq.com>
2024-03-29 00:03:03 +0800

    initial

--- uniViT_train.py
+++ uniViT_train.py
@@ -253,10 +253,13 @@ class ClassificationEvaluator(pl.LightningModule):
             **self.embed_args,
         )
         if hasattr(self.net.patch_embed.proj, 'weight'):
-            # origin_weight = self.net.patch_embed.proj.weight.clone()
+            origin_weight = self.net.patch_embed.proj.weight.clone()
             # print(origin_weight)
+            # new_weight = pi_resize_patch_embed(
+            #     patch_embed=self.state_dict["patch_embed.proj.weight"], new_patch_size=new_patch_size
+            # )
             new_weight = pi_resize_patch_embed(
-                patch_embed=self.state_dict["patch_embed.proj.weight"], new_patch_size=new_patch_size
+                patch_embed=origin_weight, new_patch_size=(new_patch_size, new_patch_size)
             )
             new_patch_embed.proj.weight = nn.Parameter(new_weight)
         if self.net.patch_embed.proj.bias is not None:
