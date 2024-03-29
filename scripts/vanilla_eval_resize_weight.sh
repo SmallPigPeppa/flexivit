74ea7c29356455fa9c80ff1b98ca0788acd0009d
lwz <905328134@qq.com>
2024-03-29 13:09:40 +0800

    initial

--- scripts/vanilla_eval_resize_weight.sh
+++ scripts/vanilla_eval_resize_weight.sh
@@ -3,7 +3,7 @@ python vanilla_eval_resize_weight.py \
   --accelerator gpu \
   --devices 8 \
   --works 4 \
-  --batch_size 256 \
+  --batch_size 64 \
   --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --model.resize_type pi \
   --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
