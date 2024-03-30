3e4c76fdbe28675e0883880dd6d518d1ade457d6
lwz <905328134@qq.com>
2024-03-30 17:51:28 +0800

    initial

---
+++ scripts/uniViT_train3_no_random_resize_4conv.sh
@@ -0,0 +1,17 @@
+python uniViT_train3_no_random_resize_4conv.py \
+  --max_epochs 5 \
+  --precision 16 \
+  --accelerator gpu \
+  --devices 8 \
+  --works 4 \
+  --batch_size 64 \
+  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+  --model.resize_type pi \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+  --model.results_path ./resize_weight.csv
+
+
+
