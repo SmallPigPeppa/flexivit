6ddf39a5c62b8dcdfc3693620c469157fe58c2c2
lwz <905328134@qq.com>
2024-03-31 01:49:01 +0800

    initial

---
+++ scripts/uniViT_4conv_eval_fix_anchor.sh
@@ -0,0 +1,19 @@
+python uniViT_4conv_eval_fix_anchor.py \
+  --max_epochs 15 \
+  --precision 16 \
+  --accelerator gpu \
+  --devices 8 \
+  --works 4 \
+  --batch_size 32 \
+  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+  --ckpt_path ckpt/uniViT/no-random-resize-4conv/last.ckpt \
+  --model.resize_type pi \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+  --model.results_path ./uniViT_4conv_eval_fix_14token.csv
+
+
+
+
