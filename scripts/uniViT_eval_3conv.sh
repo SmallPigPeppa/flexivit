f1501218a550e82e01e2acd22e543b67eeacaa8b
lwz <905328134@qq.com>
2024-03-31 14:12:08 +0800

    initial

--- scripts/uniViT_eval_3conv.sh
+++ scripts/uniViT_eval_3conv.sh
@@ -19,19 +19,19 @@ python uniViT_3conv_eval_fix_14token.py \
   --model.image_size 224
 
 
-#python uniViT_3conv_eval_fix_anchor.py \
-#  --max_epochs 15 \
-#  --precision 16 \
-#  --accelerator gpu \
-#  --devices 8 \
-#  --works 4 \
-#  --batch_size 64 \
-#  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
-#  --ckpt_path ${ckpt_path} \
-#  --model.resize_type pi \
-#  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
-#  --model.num_classes 1000 \
-#  --model.patch_size 16 \
-#  --model.image_size 224
+python uniViT_3conv_eval_fix_anchor.py \
+  --max_epochs 15 \
+  --precision 16 \
+  --accelerator gpu \
+  --devices 8 \
+  --works 4 \
+  --batch_size 64 \
+  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+  --ckpt_path ${ckpt_path} \
+  --model.resize_type pi \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224
 
 
