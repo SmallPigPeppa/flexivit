7ffcf4025a19e246da2a90f937b9339a11be23bd
lwz <905328134@qq.com>
2024-04-01 23:21:29 +0800

    initial

---
+++ L2P_scripts/L2P_train_add_random_resize_4conv_fix14token.sh
@@ -0,0 +1,18 @@
+python L2P_train_add_random_resize_4conv_fix14token.py \
+  --max_epochs 5 \
+  --precision 16 \
+  --accelerator gpu \
+  --devices 8 \
+  --works 4 \
+  --batch_size 32 \
+  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+  --model.resize_type pi \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+  --model.results_path ./resize_weight.csv
+
+
+
+
