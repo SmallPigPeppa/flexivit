2f20652e1fa9a16eb52a794453c750fc0c90d64f
lwz <905328134@qq.com>
2024-04-02 18:01:29 +0800

    initial

--- models/flex_patch_embed.py
+++ models/flex_patch_embed.py
@@ -11,23 +11,24 @@ from torch import Tensor
 from flexivit_pytorch.utils import to_2tuple
 from timm.layers.format import Format, nchw_to
 
+
 class FlexiPatchEmbed(nn.Module):
     def __init__(
-        self,
-        img_size: Union[int, Tuple[int, int]] = 240,
-        patch_size: Union[int, Tuple[int, int]] = 32,
-        grid_size: Union[int, Tuple[int, int]] = 7,
-        in_chans: int = 3,
-        embed_dim: int = 768,
-        norm_layer: Optional[nn.Module] = None,
-        flatten: bool = True,
-        bias: bool = True,
-        patch_size_seq: Sequence[int] = (8, 10, 12, 15, 16, 20, 24, 30, 40, 48),
-        patch_size_probs: Optional[Sequence[float]] = None,
-        interpolation: str = "bicubic",
-        antialias: bool = True,
-        output_fmt: Optional[str] = None,
-        strict_img_size=False
+            self,
+            img_size: Union[int, Tuple[int, int]] = 240,
+            patch_size: Union[int, Tuple[int, int]] = 32,
+            grid_size: Union[int, Tuple[int, int]] = 7,
+            in_chans: int = 3,
+            embed_dim: int = 768,
+            norm_layer: Optional[nn.Module] = None,
+            flatten: bool = True,
+            bias: bool = True,
+            patch_size_seq: Sequence[int] = (8, 10, 12, 15, 16, 20, 24, 30, 40, 48),
+            patch_size_probs: Optional[Sequence[float]] = None,
+            interpolation: str = "bicubic",
+            antialias: bool = True,
+            output_fmt: Optional[str] = None,
+            strict_img_size=False
     ) -> None:
         """2D image to patch embedding w/ flexible patch sizes
         Extended from: https://github.com/huggingface/pytorch-image-models/blob/main/timm/layers/patch_embed.py#L24
@@ -109,7 +110,7 @@ class FlexiPatchEmbed(nn.Module):
         return x_resized[0, 0, ...]
 
     def _calculate_pinv(
-        self, old_shape: Tuple[int, int], new_shape: Tuple[int, int]
+            self, old_shape: Tuple[int, int], new_shape: Tuple[int, int]
     ) -> Tensor:
         mat = []
         for i in range(np.prod(old_shape)):
@@ -143,10 +144,10 @@ class FlexiPatchEmbed(nn.Module):
         return v_resample_patch_embed(patch_embed)
 
     def forward(
-        self,
-        x: Tensor,
-        patch_size: Optional[Union[int, Tuple[int, int]]] = None,
-        return_patch_size: bool = False,
+            self,
+            x: Tensor,
+            patch_size: Optional[Union[int, Tuple[int, int]]] = None,
+            return_patch_size: bool = False,
     ) -> Union[Tensor, Tuple[Tensor, Tuple[int, int]]]:
 
         if not patch_size and not self.training:
@@ -181,3 +182,46 @@ class FlexiPatchEmbed(nn.Module):
             return x, patch_size
 
         return x
+
+
+if __name__ == '__main__':
+    # def modified(self, new_image_size=224, new_patch_size=16):
+    embed_args = {}
+    sin_chans = 3
+    embed_dim = 784
+    pre_norm = False
+    dynamic_img_pad = False
+    dynamic_img_size = True
+    if dynamic_img_size:
+        # flatten deferred until after pos embed
+        embed_args.update(dict(strict_img_size=False, output_fmt='NHWC'))
+    # self.patch_embed_4x4 = self.get_new_patch_embed(new_image_size=56, new_patch_size=4)
+    # self.patch_embed_8x8 = self.get_new_patch_embed(new_image_size=112, new_patch_size=8)
+    # self.patch_embed_16x16 = self.get_new_patch_embed(new_image_size=224, new_patch_size=16)
+    # self.patch_embed_16x16_origin = self.get_new_patch_embed(new_image_size=224, new_patch_size=16)
+    new_image_size = 56
+    new_patch_size = 4
+    in_chans = 3
+
+    # self.net.patch_embed = nn.Identity()
+
+    # def get_new_patch_embed(self, new_image_size, new_patch_size):
+    new_patch_embed = FlexiPatchEmbed(
+        img_size=new_image_size,
+        patch_size=new_patch_size,
+        in_chans=in_chans,
+        embed_dim=embed_dim,
+        bias=not pre_norm,  # disable bias if pre-norm is used (e.g. CLIP)
+        # dynamic_img_pad=self.dynamic_img_pad,
+        **embed_args,
+    )
+    x = torch.rand(size=[4, 3, 224, 224])
+    pe = new_patch_embed(x, patch_size=16)
+    print(pe.shape)
+    x = torch.rand(size=[4, 3, 112, 112])
+    pe = new_patch_embed(x, patch_size=8)
+    print(pe.shape)
+
+    x = torch.rand(size=[4, 3, 112, 224])
+    pe = new_patch_embed(x, patch_size=[8, 16])
+    print(pe.shape)
