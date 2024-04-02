42567ee47b9d1c9bd3003e9064f426b7d9ffa1a2
lwz <905328134@qq.com>
2024-04-02 19:10:22 +0800

    initial

--- L2P_scripts/L2P_eval_3conv.sh
+++ L2P_scripts/L2P_eval_3conv.sh
@@ -1,8 +1,13 @@
+#ckpt_paths=(
+#    "ckpt/L2P/add_random_resize_3conv_fix14token/last.ckpt"
+#    "ckpt/L2P/add_random_resize_3conv_fix14token_rmrand224/last.ckpt"
+#    "ckpt/L2P/add_random_resize_3conv_fix14token_rmrand224_35/last.ckpt"
+#)
+
 ckpt_paths=(
-    "ckpt/L2P/add_random_resize_3conv_fix14token/last.ckpt"
-    "ckpt/L2P/add_random_resize_3conv_fix14token_rmrand224/last.ckpt"
-    "ckpt/L2P/add_random_resize_3conv_fix14token_rmrand224_35/last.ckpt"
+    "ckpt/L2P/add_random_resize_3conv_fix14token_35/last.ckpt"
 )
+
 
 scripts=(
     "L2P_3conv_eval_fix_14token.py"
