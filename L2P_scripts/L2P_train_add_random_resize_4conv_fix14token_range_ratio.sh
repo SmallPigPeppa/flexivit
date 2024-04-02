17f08bd5a918e704901b472c3360edb087136a3d
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2024-04-03 07:09:15 +0800

    initial

--- L2P_scripts/L2P_train_add_random_resize_4conv_fix14token_range_ratio.sh
+++ L2P_scripts/L2P_train_add_random_resize_4conv_fix14token_range_ratio.sh
@@ -1,5 +1,5 @@
-python L2P_train_add_random_resize_4conv_fix14token_2range_ratio.py \
-  --max_epochs 5 \
+python L2P_train_add_random_resize_4conv_fix14token_2range_ratio_10epoch.py \
+  --max_epochs 10 \
   --precision 16 \
   --accelerator gpu \
   --devices 8 \
@@ -12,6 +12,22 @@ python L2P_train_add_random_resize_4conv_fix14token_2range_ratio.py \
   --model.patch_size 16 \
   --model.image_size 224 \
   --model.results_path ./resize_weight.csv
+
+
+#python L2P_train_add_random_resize_4conv_fix14token_2range_ratio.py \
+#  --max_epochs 5 \
+#  --precision 16 \
+#  --accelerator gpu \
+#  --devices 8 \
+#  --works 4 \
+#  --batch_size 32 \
+#  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+#  --model.resize_type pi \
+#  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+#  --model.num_classes 1000 \
+#  --model.patch_size 16 \
+#  --model.image_size 224 \
+#  --model.results_path ./resize_weight.csv
 
 #
 #python L2P_train_add_random_resize_4conv_fix14token_1.5range.py \
