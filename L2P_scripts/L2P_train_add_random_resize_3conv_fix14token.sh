5cf60be285b746aad071d2cf9a9cd0c339052c02
lwz <905328134@qq.com>
2024-04-02 15:10:16 +0800

    initial

--- L2P_scripts/L2P_train_add_random_resize_3conv_fix14token.sh
+++ L2P_scripts/L2P_train_add_random_resize_3conv_fix14token.sh
@@ -29,7 +29,24 @@
 #  --model.image_size 224 \
 #  --model.results_path ./resize_weight.csv
 
-python L2P_train_add_random_resize_3conv_fix14token_rmrand224_35.py \
+#python L2P_train_add_random_resize_3conv_fix14token_rmrand224_35.py \
+#  --max_epochs 5 \
+#  --precision 16 \
+#  --accelerator gpu \
+#  --devices 8 \
+#  --works 4 \
+#  --batch_size 32 \
+#  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+#  --model.resize_type pi \
+#  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+#  --model.num_classes 1000 \
+#  --model.patch_size 16 \
+#  --model.image_size 224 \
+#  --model.results_path ./resize_weight.csv
+
+
+
+python L2P_train_add_random_resize_3conv_fix14token_35.py \
   --max_epochs 5 \
   --precision 16 \
   --accelerator gpu \
