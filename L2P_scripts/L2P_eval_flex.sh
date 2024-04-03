a7ae87d85ad5ed5c13d010851672586a8006c350
lwz <905328134@qq.com>
2024-04-03 18:32:45 +0800

    initial

--- L2P_scripts/L2P_eval_flex.sh
+++ L2P_scripts/L2P_eval_flex.sh
@@ -8,7 +8,8 @@ ckpt_paths=(
 scripts=(
 #    "L2P_4conv_eval_fix_14token.py"
 #    "L2P_4conv_eval_fix_14token_ratio.py"
-    "L2P_4conv_eval_fix_14token_ratio2.py"
+#    "L2P_4conv_eval_fix_14token_ratio2.py"
+     "L2P_eval_flex_fix_14token_ratio.py"
 #    "L2P_4conv_eval_fix_anchor.py"
 #    "L2P_4conv_eval_fix_14token_24816.py"
 #    "L2P_4conv_eval_fix_anchor_24816.py"
