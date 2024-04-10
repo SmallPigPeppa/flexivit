d8f24bda7e4aa6effdcef11170798e5da7a96bfe
lwz <905328134@qq.com>
2024-04-11 01:13:27 +0800

    initial

--- L2P_scripts/L2P_train_add_random_resize_4conv_fix14token_2range_ratio_pvt.sh
+++ L2P_scripts/L2P_train_add_random_resize_4conv_fix14token_2range_ratio_pvt.sh
@@ -7,7 +7,7 @@ python L2P_train_add_random_resize_4conv_fix14token_2range_ratio_pvt.py \
   --batch_size 32 \
   --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --model.resize_type pi \
-  --model.weights deit_base_distilled_patch16_224.fb_in1k \
+  --model.weights pvt_v2_b3.in1k \
   --model.num_classes 1000 \
   --model.patch_size 16 \
   --model.image_size 224 \
