5154514f020c86eb4d21161e7f606bb5a7f4ac12
lwz <905328134@qq.com>
2024-04-13 00:39:17 +0800

    initial

--- L2P_scripts/vanilla_eval_resize_input.sh
+++ L2P_scripts/vanilla_eval_resize_input.sh
@@ -3,7 +3,7 @@ python vanilla_eval_resize_input_new.py \
   --accelerator gpu \
   --devices 8 \
   --works 4 \
-  --batch_size 256 \
+  --batch_size 32 \
   --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --model.resize_type pi \
   --model.weights pvt_v2_b3.in1k \
@@ -19,7 +19,7 @@ python vanilla_eval_resize_input_new.py \
   --accelerator gpu \
   --devices 8 \
   --works 4 \
-  --batch_size 256 \
+  --batch_size 32 \
   --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --model.resize_type pi \
   --model.weights mvitv2_small.fb_in1k  \
