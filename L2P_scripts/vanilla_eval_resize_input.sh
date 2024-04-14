ecd8d25f3fe6d7a92e832b7d19aba38b49d08be1
lwz <905328134@qq.com>
2024-04-14 22:50:50 +0800

    initial

--- L2P_scripts/vanilla_eval_resize_input.sh
+++ L2P_scripts/vanilla_eval_resize_input.sh
@@ -30,19 +30,34 @@
 
 
 
-python vanilla_eval_resize_input_new.py \
-  --max_epochs 1 \
-  --accelerator gpu \
-  --devices 8 \
-  --works 4 \
-  --batch_size 32 \
-  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
-  --model.resize_type pi \
-  --model.weights deit_base_distilled_patch16_224.fb_in1k  \
-  --model.num_classes 1000 \
-  --model.patch_size 16 \
-  --model.image_size 224 \
-  --model.results_path ./L2P_exp/vanilla_resize_input_deit.csv
+#python vanilla_eval_resize_input_new.py \
+#  --max_epochs 1 \
+#  --accelerator gpu \
+#  --devices 8 \
+#  --works 4 \
+#  --batch_size 32 \
+#  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+#  --model.resize_type pi \
+#  --model.weights deit_base_distilled_patch16_224.fb_in1k  \
+#  --model.num_classes 1000 \
+#  --model.patch_size 16 \
+#  --model.image_size 224 \
+#  --model.results_path ./L2P_exp/vanilla_resize_input_deit.csv
+#
+#
+#python vanilla_eval_resize_weight.py \
+#  --max_epochs 1 \
+#  --accelerator gpu \
+#  --devices 8 \
+#  --works 4 \
+#  --batch_size 64 \
+#  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+#  --model.weights deit_base_distilled_patch16_224.fb_in1k \
+#  --model.num_classes 1000 \
+#  --model.patch_size 16 \
+#  --model.image_size 224 \
+#  --model.resize_type pi \
+#  --model.results_path ./L2P_exp/vanilla_resize_weight_pi_deit.csv
 
 
 python vanilla_eval_resize_weight.py \
@@ -52,28 +67,11 @@ python vanilla_eval_resize_weight.py \
   --works 4 \
   --batch_size 64 \
   --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
-  --model.resize_type pi \
   --model.weights deit_base_distilled_patch16_224.fb_in1k \
   --model.num_classes 1000 \
   --model.patch_size 16 \
   --model.image_size 224 \
-  --model.resize_type pi \
-  --model.results_path ./L2P_exp/vanilla_resize_weight_pi_deit.csv
-
-
-python vanilla_eval_resize_weight.py \
-  --max_epochs 1 \
-  --accelerator gpu \
-  --devices 8 \
-  --works 4 \
-  --batch_size 64 \
-  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --model.resize_type interpolate \
-  --model.weights deit_base_distilled_patch16_224.fb_in1k \
-  --model.num_classes 1000 \
-  --model.patch_size 16 \
-  --model.image_size 224 \
-  --model.resize_type pi \
   --model.results_path ./L2P_exp/vanilla_resize_weight_bilinear_deit.csv
 
 
