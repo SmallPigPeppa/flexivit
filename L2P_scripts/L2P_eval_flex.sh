94adb13786296a54bd9b59c07f1d513e07fbb39c
lwz <905328134@qq.com>
2024-04-14 21:34:30 +0800

    initial

--- L2P_scripts/L2P_eval_flex.sh
+++ L2P_scripts/L2P_eval_flex.sh
@@ -1,6 +1,7 @@
 ckpt_paths=(
 #    "ckpt/L2P/add_random_resize_4conv_fix14token_24816_ratio/last.ckpt"
      "ckpt/L2P/add_random_resize_4conv_fix14token_2range_ratio/last.ckpt"
+#    "ckpt/L2P/add_random_resize_4conv_fix14token_2range_ratio/last.ckpt"
 #    "ckpt/L2P/add_random_resize_4conv_fix14token_2range/last.ckpt"
 #    "ckpt/L2P/add_random_resize_4conv_fix14token_24816/last.ckpt"
 )
@@ -9,7 +10,9 @@ scripts=(
 #    "L2P_4conv_eval_fix_14token.py"
 #    "L2P_4conv_eval_fix_14token_ratio.py"
 #    "L2P_4conv_eval_fix_14token_ratio2.py"
-     "L2P_eval_flex_fix_14token_ratio.py"
+#     "L2P_eval_flex_fix_14token_ratio.py"
+     "L2P_eval_bilinear_fix_14token_ratio.py"
+     "L2P_eval_imgresize_fix_14token_ratio.py"
 #    "L2P_4conv_eval_fix_anchor.py"
 #    "L2P_4conv_eval_fix_14token_24816.py"
 #    "L2P_4conv_eval_fix_anchor_24816.py"
