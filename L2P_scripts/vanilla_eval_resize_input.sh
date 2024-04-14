19992276eadd153f136704c15ca22b648f4668dc
lwz <905328134@qq.com>
2024-04-14 21:05:15 +0800

    initial

--- L2P_scripts/vanilla_eval_resize_input.sh
+++ L2P_scripts/vanilla_eval_resize_input.sh
@@ -14,6 +14,22 @@
 
 
 
+#python vanilla_eval_resize_input_new.py \
+#  --max_epochs 1 \
+#  --accelerator gpu \
+#  --devices 8 \
+#  --works 4 \
+#  --batch_size 32 \
+#  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+#  --model.resize_type pi \
+#  --model.weights mvitv2_small.fb_in1k  \
+#  --model.num_classes 1000 \
+#  --model.patch_size 16 \
+#  --model.image_size 224 \
+#  --model.results_path ./L2P_exp/vanilla_resize_input_mvit.csv
+
+
+
 python vanilla_eval_resize_input_new.py \
   --max_epochs 1 \
   --accelerator gpu \
@@ -22,10 +38,43 @@ python vanilla_eval_resize_input_new.py \
   --batch_size 32 \
   --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --model.resize_type pi \
-  --model.weights mvitv2_small.fb_in1k  \
+  --model.weights deit_base_distilled_patch16_224.fb_in1k  \
   --model.num_classes 1000 \
   --model.patch_size 16 \
   --model.image_size 224 \
-  --model.results_path ./L2P_exp/vanilla_resize_input_mvit.csv
+  --model.results_path ./L2P_exp/vanilla_resize_input_deit.csv
+
+
+python vanilla_eval_resize_weight.py \
+  --max_epochs 1 \
+  --accelerator gpu \
+  --devices 8 \
+  --works 4 \
+  --batch_size 64 \
+  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+  --model.resize_type pi \
+  --model.weights deit_base_distilled_patch16_224.fb_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+  --model.resize_type pi \
+  --model.results_path ./L2P_exp/vanilla_resize_weight_pi_deit.csv
+
+
+python vanilla_eval_resize_weight.py \
+  --max_epochs 1 \
+  --accelerator gpu \
+  --devices 8 \
+  --works 4 \
+  --batch_size 64 \
+  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+  --model.resize_type interpolate \
+  --model.weights deit_base_distilled_patch16_224.fb_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+  --model.resize_type pi \
+  --model.results_path ./L2P_exp/vanilla_resize_weight_bilinear_deit.csv
+
 
 
