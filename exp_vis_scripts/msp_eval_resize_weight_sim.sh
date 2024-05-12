6466c2966c4705929a4d6b186d4f061cefe82764
lwz <905328134@qq.com>
2024-05-12 21:35:35 +0800

    initial

---
+++ exp_vis_scripts/msp_eval_resize_weight_sim.sh
@@ -0,0 +1,17 @@
+python vv_exp_vis_msp_eval_resize_weight_sim.py \
+  --max_epochs 1 \
+  --accelerator gpu \
+  --devices 1 \
+  --works 1 \
+  --batch_size 1 \
+  --root /ppio_net0/torch_ds/imagenet \
+  --ckpt_path ckpt/MSP/add_random_resize_4conv_fix14token_2range/last.ckpt \
+  --model.resize_type pi \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+
+
+
+
