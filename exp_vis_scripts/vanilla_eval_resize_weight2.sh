44c378555ed04f5e86305a985d05007b04fb368f
lwz <905328134@qq.com>
2024-05-12 16:44:07 +0800

    initial

--- exp_vis_scripts/vanilla_eval_resize_weight2.sh
+++ exp_vis_scripts/vanilla_eval_resize_weight2.sh
@@ -6,7 +6,7 @@ python vv_exp_vis_vanilla_eval_resize_weight2.py \
   --batch_size 64 \
   --root /ppio_net0/torch_ds/imagenet \
   --ckpt_path ckpt/MSP/add_random_resize_4conv_fix14token_2range/last.ckpt \
-  --model.resize_type interpolate \
+  --model.resize_type pi \
   --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
   --model.num_classes 1000 \
   --model.patch_size 16 \
