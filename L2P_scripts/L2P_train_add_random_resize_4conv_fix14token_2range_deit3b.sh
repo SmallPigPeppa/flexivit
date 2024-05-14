628ecabf197a66ac72c8a815ad3f3c3842949f3b
lwz <905328134@qq.com>
2024-05-14 21:50:33 +0800

    initial

--- L2P_scripts/L2P_train_add_random_resize_4conv_fix14token_2range_deit3b.sh
+++ L2P_scripts/L2P_train_add_random_resize_4conv_fix14token_2range_deit3b.sh
@@ -7,7 +7,7 @@ python L2P_train_add_random_resize_4conv_fix14token_2range_deit3b.py \
   --devices 8 \
   --works 4 \
   --batch_size 32 \
-  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+  --root /ppio_net0/torch_ds/imagenet \
   --model.resize_type pi \
   --model.weights deit3_base_patch16_224.fb_in22k_ft_in1k \
   --model.num_classes 1000 \
@@ -15,20 +15,10 @@ python L2P_train_add_random_resize_4conv_fix14token_2range_deit3b.py \
   --model.image_size 224 \
   --model.results_path ./resize_weight.csv
 
+python /ppio_net0/code/openapi.py stop 4fca613d27e9ae5f
 
-#python L2P_train_add_random_resize_4conv_fix14token_1.5range.py \
-#  --max_epochs 5 \
-#  --precision 16 \
-#  --accelerator gpu \
-#  --devices 8 \
-#  --works 4 \
-#  --batch_size 32 \
-#  --root /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
-#  --model.resize_type pi \
-#  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
-#  --model.num_classes 1000 \
-#  --model.patch_size 16 \
-#  --model.image_size 224 \
-#  --model.results_path ./resize_weight.csv
+
+
+
 
 
