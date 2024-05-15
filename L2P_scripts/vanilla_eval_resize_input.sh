f14413751217a17a407d79a1d02f4784d912682d
lwz <905328134@qq.com>
2024-05-15 18:53:15 +0800

    initial

--- L2P_scripts/vanilla_eval_resize_input.sh
+++ L2P_scripts/vanilla_eval_resize_input.sh
@@ -76,19 +76,48 @@
 
 
 
-python vanilla_eval_resize_weight.py \
-  --max_epochs 1 \
-  --accelerator gpu \
-  --devices 1 \
-  --works 4 \
-  --batch_size 64 \
-  --root /ppio_net0/torch_ds/imagenet \
-  --model.weights deit3_base_patch16_224.fb_in22k_ft_in1k \
-  --model.num_classes 1000 \
-  --model.patch_size 16 \
-  --model.image_size 224 \
-  --model.resize_type pi \
-  --model.results_path ./L2P_exp/vanilla_resize_weight_pi_deit3b.csv
+#python vanilla_eval_resize_weight.py \
+#  --max_epochs 1 \
+#  --accelerator gpu \
+#  --devices 1 \
+#  --works 4 \
+#  --batch_size 64 \
+#  --root /ppio_net0/torch_ds/imagenet \
+#  --model.weights deit3_base_patch16_224.fb_in22k_ft_in1k \
+#  --model.num_classes 1000 \
+#  --model.patch_size 16 \
+#  --model.image_size 224 \
+#  --model.resize_type pi \
+#  --model.results_path ./L2P_exp/vanilla_resize_weight_pi_deit3b.csv
+#
+#python vanilla_eval_resize_weight.py \
+#  --max_epochs 1 \
+#  --accelerator gpu \
+#  --devices 1 \
+#  --works 4 \
+#  --batch_size 64 \
+#  --root /ppio_net0/torch_ds/imagenet \
+#  --model.weights deit3_base_patch16_224.fb_in22k_ft_in1k \
+#  --model.num_classes 1000 \
+#  --model.patch_size 16 \
+#  --model.image_size 224 \
+#  --model.resize_type interpolate \
+#  --model.results_path ./L2P_exp/vanilla_resize_weight_bilinear_deit3b.csv
+
+
+#python vanilla_eval_resize_weight.py \
+#  --max_epochs 1 \
+#  --accelerator gpu \
+#  --devices 1 \
+#  --works 4 \
+#  --batch_size 64 \
+#  --root /ppio_net0/torch_ds/imagenet \
+#  --model.weights deit3_base_patch16_224.fb_in22k_ft_in1k \
+#  --model.num_classes 1000 \
+#  --model.patch_size 16 \
+#  --model.image_size 224 \
+#  --model.resize_type pi \
+#  --model.results_path ./L2P_exp/vanilla_resize_weight_pi_deit3b.csv
 
 python vanilla_eval_resize_weight.py \
   --max_epochs 1 \
@@ -97,12 +126,26 @@ python vanilla_eval_resize_weight.py \
   --works 4 \
   --batch_size 64 \
   --root /ppio_net0/torch_ds/imagenet \
-  --model.weights deit3_base_patch16_224.fb_in22k_ft_in1k \
+  --model.weights deit_base_distilled_patch16_224.fb_in1k \
   --model.num_classes 1000 \
   --model.patch_size 16 \
   --model.image_size 224 \
-  --model.resize_type interpolate \
-  --model.results_path ./L2P_exp/vanilla_resize_weight_bilinear_deit3b.csv
+  --model.resize_type pi \
+  --model.results_path ./L2P_exp/vanilla_resize_weight_pi_deit.csv
+
+#python vanilla_eval_resize_weight.py \
+#  --max_epochs 1 \
+#  --accelerator gpu \
+#  --devices 1 \
+#  --works 4 \
+#  --batch_size 64 \
+#  --root /ppio_net0/torch_ds/imagenet \
+#  --model.weights deit3_base_patch16_224.fb_in22k_ft_in1k \
+#  --model.num_classes 1000 \
+#  --model.patch_size 16 \
+#  --model.image_size 224 \
+#  --model.resize_type interpolate \
+#  --model.results_path ./L2P_exp/vanilla_resize_weight_bilinear_deit3b.csv
 
 
 
