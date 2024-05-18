f7cac84a3ca24890119c5065c9b3f7546a2e7cbf
lwz <905328134@qq.com>
2024-05-19 02:11:13 +0800

    initial

--- L2P_scripts/L2P_eval_4conv_ratio8.sh
+++ L2P_scripts/L2P_eval_4conv_ratio8.sh
@@ -9,7 +9,7 @@ scripts=(
 #    "L2P_4conv_eval_fix_14token.py"
 #    "L2P_4conv_eval_fix_14token_ratio.py"
 #     "L2P_4conv_eval_fix_14token_ratio2.py"
-     "L2P_4conv_eval_fix_14token_ratio16.py"
+     "L2P_4conv_eval_fix_14token_ratio8.py"
 #    "L2P_4conv_eval_fix_anchor.py"
 #    "L2P_4conv_eval_fix_14token_24816.py"
 #    "L2P_4conv_eval_fix_anchor_24816.py"
