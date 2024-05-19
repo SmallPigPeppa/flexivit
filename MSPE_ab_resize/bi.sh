f8d26fb7fc1945d5dd8843f6e4191a88e91eaf75
lwz <905328134@qq.com>
2024-05-20 00:20:52 +0800

    initial

---
+++ MSPE_ab_resize/bi.sh
@@ -0,0 +1,45 @@
+python L2P_train_add_random_resize_4conv_fix14token_2range_ratio_bilinear.py \
+  --max_epochs 5 \
+  --precision 16 \
+  --accelerator gpu \
+  --devices 8 \
+  --works 8 \
+  --batch_size 32 \
+  --root /ppio_net0/torch_ds/imagenet \
+  --model.resize_type pi \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+  --model.results_path ./resize_weight.csv
+
+python L2P_train_add_random_resize_4conv_fix14token_2range_ratio_nearest.py \
+  --max_epochs 5 \
+  --precision 16 \
+  --accelerator gpu \
+  --devices 8 \
+  --works 8 \
+  --batch_size 32 \
+  --root /ppio_net0/torch_ds/imagenet \
+  --model.resize_type pi \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+  --model.results_path ./resize_weight.csv
+
+python L2P_train_add_random_resize_4conv_fix14token_2range_ratio_bicubic.py \
+  --max_epochs 5 \
+  --precision 16 \
+  --accelerator gpu \
+  --devices 8 \
+  --works 8 \
+  --batch_size 32 \
+  --root /ppio_net0/torch_ds/imagenet \
+  --model.resize_type pi \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+  --model.results_path ./resize_weight.csv
+
