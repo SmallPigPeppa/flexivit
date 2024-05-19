51a56ee7c374a949530cdaaa000a4dc0b4bc2680
lwz <905328134@qq.com>
2024-05-19 17:02:48 +0800

    initial

--- MSPE_ablation_scripts/L2P_train_add_random_resize_4conv_fix14token_2range.sh
+++ MSPE_ablation_scripts/L2P_train_add_random_resize_4conv_fix14token_2range.sh
@@ -1,11 +1,9 @@
-
-
 python L2P_train_add_random_resize_4conv_fix14token_2range.py \
   --max_epochs 5 \
   --precision 16 \
   --accelerator gpu \
   --devices 8 \
-  --works 4 \
+  --works 8 \
   --batch_size 32 \
   --root /ppio_net0/torch_ds/imagenet \
   --model.resize_type pi \
@@ -15,20 +13,5 @@ python L2P_train_add_random_resize_4conv_fix14token_2range.py \
   --model.image_size 224 \
   --model.results_path ./resize_weight.csv
 
-
-#python L2P_train_add_random_resize_4conv_fix14token_1.5range.py \
-#  --max_epochs 5 \
-#  --precision 16 \
-#  --accelerator gpu \
-#  --devices 8 \
-#  --works 4 \
-#  --batch_size 32 \
-#  --root /ppio_net0/torch_ds/imagenet \
-#  --model.resize_type pi \
-#  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
-#  --model.num_classes 1000 \
-#  --model.patch_size 16 \
-#  --model.image_size 224 \
-#  --model.results_path ./resize_weight.csv
 
 
