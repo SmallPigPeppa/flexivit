c733a0d8cc39e8280770d6e6fbf073ed15f57fe4
lwz <905328134@qq.com>
2024-05-20 00:12:10 +0800

    initial

--- L2P_train_add_random_resize_4conv_fix14token_2range_ratio_1epoch.py
+++ L2P_train_add_random_resize_4conv_fix14token_2range_ratio_1epoch.py
@@ -198,7 +198,8 @@ class ClassificationEvaluator(pl.LightningModule):
             warmup_start_lr=0.01 * self.lr,
             eta_min=0.01 * self.lr,
         )
-        return [optimizer], [scheduler]
+        # return [optimizer], [scheduler]
+        return [optimizer]
 
     def forward_features(self, x: torch.Tensor) -> torch.Tensor:
         x = self.net.patch_embed(x)
@@ -337,10 +338,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     args["logger"] = False  # Disable saving logging artifacts
 
-    wandb_logger = WandbLogger(name='ab-1epoch-ratio', project='MSPE-ab',
+    wandb_logger = WandbLogger(name='ab-1epoch-ratio-new', project='MSPE-ab',
                                entity='pigpeppa', offline=False)
     checkpoint_callback = ModelCheckpoint(monitor="val_acc_16x16", mode="max",
-                                          dirpath='ckpt/MSPE-ab/1-epoch-ratio',
+                                          dirpath='ckpt/MSPE-ab/1-epoch-ratio-new',
                                           save_top_k=1,
                                           save_last=True)
     trainer = pl.Trainer.from_argparse_args(args, logger=wandb_logger, callbacks=[checkpoint_callback])
