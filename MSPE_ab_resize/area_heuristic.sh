9cf0bc7bdecc9a41286205409bfce6c2405677e7
lwz <905328134@qq.com>
2024-05-19 21:34:20 +0800

    initial

---
+++ MSPE_ab_resize/area_heuristic.sh
@@ -0,0 +1,15 @@
+python L2P_train_add_random_resize_4conv_fix14token_2range_ratio_area_heuristic.py \
+  --max_epochs 5 \
+  --precision 16 \
+  --accelerator gpu \
+  --devices 8 \
+  --works 8 \
+  --batch_size 32 \
+  --root /ppio_net0/torch_ds/imagenet \
+  --model.resize_type pi \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+  --model.results_path ./resize_weight.csv
+
