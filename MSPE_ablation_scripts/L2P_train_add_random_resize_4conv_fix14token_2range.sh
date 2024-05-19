a1350791805d6979403dbed16957b5523e93ed39
lwz <905328134@qq.com>
2024-05-19 17:00:57 +0800

    initial

--- MSPE_ablation_scripts/L2P_train_add_random_resize_4conv_fix14token_2range.sh
+++ MSPE_ablation_scripts/L2P_train_add_random_resize_4conv_fix14token_2range.sh
@@ -1,6 +1,6 @@
 
 
-python L2P_train_add_random_resize_4conv_fix14token_2range_deit.py \
+python L2P_train_add_random_resize_4conv_fix14token_2range.py \
   --max_epochs 5 \
   --precision 16 \
   --accelerator gpu \
@@ -9,7 +9,7 @@ python L2P_train_add_random_resize_4conv_fix14token_2range_deit.py \
   --batch_size 32 \
   --root /ppio_net0/torch_ds/imagenet \
   --model.resize_type pi \
-  --model.weights deit_base_distilled_patch16_224.fb_in1k \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
   --model.num_classes 1000 \
   --model.patch_size 16 \
   --model.image_size 224 \
--- MSPE_ablation_scripts/L2P_train_add_random_resize_4conv_fix14token_2range_ratio.sh
+++ MSPE_ablation_scripts/L2P_train_add_random_resize_4conv_fix14token_2range_ratio.sh
@@ -1,5 +1,5 @@
-python L2P_train_add_random_resize_4conv_fix14token_2range_ratio_deit.py \
-  --max_epochs 5 \
+python L2P_train_add_random_resize_4conv_fix14token_2range_ratio_10epoch.py \
+  --max_epochs 10 \
   --precision 16 \
   --accelerator gpu \
   --devices 8 \
@@ -7,7 +7,7 @@ python L2P_train_add_random_resize_4conv_fix14token_2range_ratio_deit.py \
   --batch_size 32 \
   --root /ppio_net0/torch_ds/imagenet \
   --model.resize_type pi \
-  --model.weights deit_base_distilled_patch16_224.fb_in1k \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
   --model.num_classes 1000 \
   --model.patch_size 16 \
   --model.image_size 224 \
