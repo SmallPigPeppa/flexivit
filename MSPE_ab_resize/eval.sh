09a163157c160e577cbb1a9465b0bf0249ff0f89
lwz <905328134@qq.com>
2024-05-20 10:45:03 +0800

    initial

--- MSPE_ab_resize/eval.sh
+++ MSPE_ab_resize/eval.sh
@@ -25,3 +25,33 @@ for ckpt_path in "${ckpt_paths[@]}"; do
             --model.image_size 224
     done
 done
+
+
+
+#ckpt_paths=(
+#    "ckpt/MSPE-ab/area_heuristic/last.ckpt"
+#)
+#scripts=(
+#     "L2P_4conv_eval_fix_14token_resize_mode.py"
+#)
+#
+#
+#for ckpt_path in "${ckpt_paths[@]}"; do
+#    for script in "${scripts[@]}"; do
+#        python ${script} \
+#            --max_epochs 15 \
+#            --precision 16 \
+#            --accelerator gpu \
+#            --devices 1 \
+#            --works 8 \
+#            --batch_size 64 \
+#            --root /ppio_net0/torch_ds/imagenet \
+#            --ckpt_path ${ckpt_path} \
+#            --mode area \
+#            --model.resize_type pi \
+#            --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+#            --model.num_classes 1000 \
+#            --model.patch_size 16 \
+#            --model.image_size 224
+#    done
+#done
