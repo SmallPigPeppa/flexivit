b4db884721627d63692b5b9ec349cd07c5e9a5d7
lwz <905328134@qq.com>
2024-08-01 16:21:44 +0800

    initial

--- MSPE_rebuttal/vanilla_eval_resize_input_vitb21k.sh
+++ MSPE_rebuttal/vanilla_eval_resize_input_vitb21k.sh
@@ -13,12 +13,12 @@
 #  --model.results_path ./L2P_exp/vanilla_resize_weight_pi_deit3b.csv
 
 
-python vanilla_eval_resize_weight_vitb21k.py \
+python vanilla_eval_resize_weight_vitb21k_rebuttal.py \
   --max_epochs 1 \
   --accelerator gpu \
   --devices 1 \
   --works 4 \
-  --batch_size 64 \
+  --batch_size 256 \
   --root /ppio_net0/torch_ds/imagenet \
   --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
   --model.num_classes 1000 \
