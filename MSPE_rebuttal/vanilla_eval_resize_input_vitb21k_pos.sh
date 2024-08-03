5f83562f27865bc756f8e00b8d20506a95fb6b23
lwz <905328134@qq.com>
2024-08-03 23:04:45 +0800

    initial

---
+++ MSPE_rebuttal/vanilla_eval_resize_input_vitb21k_pos.sh
@@ -0,0 +1,22 @@
+python vanilla_eval_resize_weight_vitb21k_rebuttal_pos.py \
+  --max_epochs 1 \
+  --accelerator gpu \
+  --devices 1 \
+  --works 4 \
+  --batch_size 64 \
+  --root /ppio_net0/torch_ds/imagenet \
+  --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
+  --model.num_classes 1000 \
+  --model.patch_size 16 \
+  --model.image_size 224 \
+  --model.resize_type pi \
+  --model.results_path ./L2P_exp/vit_base_patch16_224.augreg2_in21k_ft_in1k_pi_pos.csv
+
+
+
+
+
+
+
+
+
