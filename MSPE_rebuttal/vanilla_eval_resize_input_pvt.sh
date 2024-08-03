c8200bc171537d1fc1ed9b809f3bdaf3289a9b7c
lwz <905328134@qq.com>
2024-08-03 18:53:14 +0800

    initial

--- MSPE_rebuttal/vanilla_eval_resize_input_pvt.sh
+++ MSPE_rebuttal/vanilla_eval_resize_input_pvt.sh
@@ -13,19 +13,19 @@ python L2P_4conv_eval_fix_14token_pvt_piresize.py \
   --model.results_path ./L2P_exp/vanilla_resize_weight_pi_pvt.csv
 
 
-python L2P_4conv_eval_fix_14token_pvt_bilinear.py \
-  --max_epochs 1 \
-  --accelerator gpu \
-  --devices 1 \
-  --works 4 \
-  --batch_size 64 \
-  --root /ppio_net0/torch_ds/imagenet \
-  --model.weights pvt_v2_b3.in1k \
-  --model.num_classes 1000 \
-  --model.patch_size 16 \
-  --model.image_size 224 \
-  --model.resize_type interpolate \
-  --model.results_path ./L2P_exp/vanilla_resize_weight_bilinear_pvt.csv
+#python L2P_4conv_eval_fix_14token_pvt_bilinear.py \
+#  --max_epochs 1 \
+#  --accelerator gpu \
+#  --devices 1 \
+#  --works 4 \
+#  --batch_size 64 \
+#  --root /ppio_net0/torch_ds/imagenet \
+#  --model.weights pvt_v2_b3.in1k \
+#  --model.num_classes 1000 \
+#  --model.patch_size 16 \
+#  --model.image_size 224 \
+#  --model.resize_type interpolate \
+#  --model.results_path ./L2P_exp/vanilla_resize_weight_bilinear_pvt.csv
 
 
 
