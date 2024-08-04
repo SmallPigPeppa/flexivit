8d72778426abb9ed684cd14b242d928bdd012b8e
lwz <905328134@qq.com>
2024-08-04 19:19:11 +0800

    initial

--- vanilla_eval_resize_weight_vitb21k_rebuttal_mspe_jt.py
+++ vanilla_eval_resize_weight_vitb21k_rebuttal_mspe_jt.py
@@ -19,6 +19,8 @@ from torch import nn
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
+from models.flex_patch_embed import FlexiPatchEmbed
+
 
 class ClassificationEvaluator(pl.LightningModule):
     def __init__(
@@ -87,62 +89,64 @@ class ClassificationEvaluator(pl.LightningModule):
         self.net.load_state_dict(state_dict, strict=True)
 
         # Define metrics
-        self.acc = Accuracy(num_classes=self.num_classes, task="multiclass", top_k=1)
+        self.acc0 = Accuracy(num_classes=self.num_classes, task="multiclass", top_k=1)
+        self.acc1 = Accuracy(num_classes=self.num_classes, task="multiclass", top_k=1)
+        self.acc2 = Accuracy(num_classes=self.num_classes, task="multiclass", top_k=1)
+        self.acc3 = Accuracy(num_classes=self.num_classes, task="multiclass", top_k=1)
 
         # Define loss
         self.loss_fn = CrossEntropyLoss()
 
-        self.pos_embed_height = nn.Parameter(torch.randn(self.image_size // self.patch_size, self.net.embed_dim))
-        self.pos_embed_width = nn.Parameter(torch.randn(self.image_size // self.patch_size, self.net.embed_dim))
-        self.pos_height = self.image_size // self.patch_size
-        self.pos_width = self.image_size // self.patch_size
+        self.modified()
 
-    def _pos_embed_learn2D(self, x, patch_positions=None):
-        batch_size, num_patches, _ = x.shape
+        # self.pos_embed_height = nn.Parameter(torch.randn(self.image_size // self.patch_size, self.net.embed_dim))
+        # self.pos_embed_width = nn.Parameter(torch.randn(self.image_size // self.patch_size, self.net.embed_dim))
+        # self.pos_height = self.image_size // self.patch_size
+        # self.pos_width = self.image_size // self.patch_size
 
-        if patch_positions is None:
-            # Generate patch positions based on the assumed grid layout
-            assert num_patches == self.pos_height * self.pos_width, "Number of patches does not match the specified grid dimensions"
-            patch_positions = torch.cartesian_prod(torch.arange(self.pos_height), torch.arange(self.pos_width))
-            patch_positions = patch_positions.repeat(batch_size, 1, 1).view(batch_size, self.pos_height, self.pos_width,
-                                                                            2).to(
-                self.device)
-
-        # Unbind patch positions into height and width indices
-        h_indices, w_indices = patch_positions.unbind(dim=-1)
-
-        # Retrieve positional embeddings
-        h_pos = self.pos_embed_height[h_indices]
-        w_pos = self.pos_embed_width[w_indices]
-
-        pos_embed = h_pos + w_pos
-
-        # Combine embeddings and positional encodings
-        to_cat = []
-        if self.net.cls_token is not None:
-            to_cat.append(self.net.cls_token.expand(batch_size, -1, -1))
-        if self.net.reg_token is not None:
-            to_cat.append(self.net.reg_token.expand(batch_size, -1, -1))
-
-        no_embed_class = True
-        # if self.net.no_embed_class:
-        if no_embed_class:
-            x = x + pos_embed.view(batch_size, num_patches, -1)  # Add pos_embed first
-            if to_cat:
-                x = torch.cat(to_cat + [x], dim=1)
-        else:
-            if to_cat:
-                x = torch.cat(to_cat + [x], dim=1)
-            x = x + pos_embed.view(batch_size, num_patches, -1)  # Add pos_embed after concat
-
-        return self.net.pos_drop(x)
+    # def _pos_embed_learn2D(self, x, patch_positions=None):
+    #     batch_size, num_patches, _ = x.shape
+    #
+    #     if patch_positions is None:
+    #         # Generate patch positions based on the assumed grid layout
+    #         assert num_patches == self.pos_height * self.pos_width, "Number of patches does not match the specified grid dimensions"
+    #         patch_positions = torch.cartesian_prod(torch.arange(self.pos_height), torch.arange(self.pos_width))
+    #         patch_positions = patch_positions.repeat(batch_size, 1, 1).view(batch_size, self.pos_height, self.pos_width,
+    #                                                                         2).to(
+    #             self.device)
+    #
+    #     # Unbind patch positions into height and width indices
+    #     h_indices, w_indices = patch_positions.unbind(dim=-1)
+    #
+    #     # Retrieve positional embeddings
+    #     h_pos = self.pos_embed_height[h_indices]
+    #     w_pos = self.pos_embed_width[w_indices]
+    #
+    #     pos_embed = h_pos + w_pos
+    #
+    #     # Combine embeddings and positional encodings
+    #     to_cat = []
+    #     if self.net.cls_token is not None:
+    #         to_cat.append(self.net.cls_token.expand(batch_size, -1, -1))
+    #     if self.net.reg_token is not None:
+    #         to_cat.append(self.net.reg_token.expand(batch_size, -1, -1))
+    #
+    #     no_embed_class = True
+    #     # if self.net.no_embed_class:
+    #     if no_embed_class:
+    #         x = x + pos_embed.view(batch_size, num_patches, -1)  # Add pos_embed first
+    #         if to_cat:
+    #             x = torch.cat(to_cat + [x], dim=1)
+    #     else:
+    #         if to_cat:
+    #             x = torch.cat(to_cat + [x], dim=1)
+    #         x = x + pos_embed.view(batch_size, num_patches, -1)  # Add pos_embed after concat
+    #
+    #     return self.net.pos_drop(x)
 
     def forward_features(self, x: torch.Tensor) -> torch.Tensor:
         x = self.net.patch_embed(x)
-        import pdb;
-        # pdb.set_trace()
-        # x = self.net._pos_embed(x)
-        x = self._pos_embed_learn2D(x)
+        x = self.net._pos_embed(x)
         x = self.net.patch_drop(x)
         x = self.net.norm_pre(x)
         if self.net.grad_checkpointing and not torch.jit.is_scripting():
@@ -168,28 +172,61 @@ class ClassificationEvaluator(pl.LightningModule):
         x = self.forward_head(x)
         return x
 
-    def share_step(self, batch, _):
-        x, y = batch
-        x = F.interpolate(x, size=self.image_size, mode='bilinear')
+    def ms_forward(self, x: torch.Tensor) -> torch.Tensor:
+        x_4x4 = F.interpolate(x, size=56, mode='bilinear')
+        x_4x4 = self.patch_embed_4x4(x_4x4, patch_size=4)
+        logits_4x4 = self.forward(x_4x4)
 
-        # Pass through network
-        pred = self(x)
-        loss = self.loss_fn(pred, y)
+        x_8x8 = F.interpolate(x, size=112, mode='bilinear')
+        x_8x8 = self.patch_embed_8x8(x_8x8, patch_size=8)
+        logits_8x8 = self.forward(x_8x8)
 
-        # Get accuracy
-        acc = self.acc(pred, y)
+        x_12x12 = F.interpolate(x, size=168, mode='bilinear')
+        x_12x12 = self.patch_embed_12x12(x_12x12, patch_size=12)
+        logits_12x12 = self.forward(x_12x12)
 
-        return loss, acc
+        x_16x16 = F.interpolate(x, size=224, mode='bilinear')
+        x_16x16 = self.patch_embed_16x16(x_16x16, patch_size=16)
+        logits_16x16 = self.forward(x_16x16)
+
+        return logits_4x4, logits_8x8, logits_12x12, logits_16x16
 
     def training_step(self, batch, batch_idx):
-        loss, acc = self.share_step(batch, batch_idx)
-        self.log_dict({'train/loss': loss, 'train/acc': acc}, sync_dist=True, on_epoch=True)
-        return loss
+        out_dict = self.share_step(batch, batch_idx)
+        log_dict = {f'train/{key}': value for key, value in out_dict.items()}
+        self.log_dict(log_dict, on_step=False, sync_dist=True, on_epoch=True)
+        return out_dict['loss']
 
     def validation_step(self, batch, batch_idx):
-        loss, acc = self.share_step(batch, batch_idx)
-        self.log_dict({'val/loss': loss, 'val/acc': acc}, sync_dist=True, on_epoch=True)
-        return loss
+        out_dict = self.share_step(batch, batch_idx)
+        log_dict = {f'val/{key}': value for key, value in out_dict.items()}
+        self.log_dict(log_dict, on_step=False, sync_dist=True, on_epoch=True)
+        return out_dict['loss']
+
+    def share_step(self, batch, batch_idx):
+        x, y = batch
+        logits_4x4, logits_8x8, logits_12x12, logits_16x16 = self.ms_forward(x)
+        loss_4x4 = self.loss_fn(logits_4x4, y)
+        acc_4x4 = self.acc0(logits_4x4, y)
+        loss_8x8 = self.loss_fn(logits_8x8, y)
+        acc_8x8 = self.acc1(logits_8x8, y)
+        loss_12x12 = self.loss_fn(logits_12x12, y)
+        acc_12x12 = self.acc2(logits_12x12, y)
+        loss_16x16 = self.loss_fn(logits_16x16, y)
+        acc_16x16 = self.acc3(logits_16x16, y)
+        loss = loss_4x4 + loss_8x8 + loss_12x12 + loss_16x16
+        out_dict = {'loss': loss,
+                    'loss_4x4': loss_4x4,
+                    'loss_8x8': loss_8x8,
+                    'loss_12x12': loss_12x12,
+                    'loss_16x16': loss_16x16,
+                    'acc_4x4': acc_4x4,
+                    'acc_8x8': acc_8x8,
+                    'acc_12x12': acc_12x12,
+                    'acc_16x16': acc_16x16
+                    }
+        return out_dict
+
 
     def test_step(self, batch, batch_idx):
         loss, acc = self.share_step(batch, batch_idx)
@@ -241,6 +278,47 @@ class ClassificationEvaluator(pl.LightningModule):
         )
         return [optimizer], [scheduler]
 
+    def modified(self, ):
+        self.embed_args = {}
+        self.in_chans = 3
+        self.embed_dim = self.net.num_features
+        self.pre_norm = False
+        self.dynamic_img_pad = False
+        if self.net.dynamic_img_size:
+            # flatten deferred until after pos embed
+            self.embed_args.update(dict(strict_img_size=False, output_fmt='NHWC'))
+        self.patch_embed_4x4 = self.get_new_patch_embed(new_image_size=56, new_patch_size=4)
+        self.patch_embed_8x8 = self.get_new_patch_embed(new_image_size=112, new_patch_size=8)
+        self.patch_embed_12x12 = self.get_new_patch_embed(new_image_size=168, new_patch_size=12)
+        self.patch_embed_16x16 = self.get_new_patch_embed(new_image_size=224, new_patch_size=16)
+        self.patch_embed_16x16_origin = self.get_new_patch_embed(new_image_size=224, new_patch_size=16)
+        self.net.patch_embed = nn.Identity()
+
+    def get_new_patch_embed(self, new_image_size, new_patch_size):
+        new_patch_embed = FlexiPatchEmbed(
+            img_size=new_image_size,
+            patch_size=new_patch_size,
+            in_chans=self.in_chans,
+            embed_dim=self.embed_dim,
+            bias=not self.pre_norm,  # disable bias if pre-norm is used (e.g. CLIP)
+            # dynamic_img_pad=self.dynamic_img_pad,
+            **self.embed_args,
+        )
+        if hasattr(self.net.patch_embed.proj, 'weight'):
+            origin_weight = self.net.patch_embed.proj.weight.clone().detach()
+            new_weight = pi_resize_patch_embed(
+                patch_embed=origin_weight,
+                new_patch_size=(new_patch_size, new_patch_size)
+            )
+            new_patch_embed.proj.weight = nn.Parameter(new_weight, requires_grad=True)
+        if self.net.patch_embed.proj.bias is not None:
+            new_patch_embed.proj.bias = nn.Parameter(
+                self.net.patch_embed.proj.bias.clone().detach(),
+                requires_grad=True
+            )
+
+        return new_patch_embed
+
 
 if __name__ == "__main__":
     parser = LightningArgumentParser()
@@ -252,9 +330,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     args["logger"] = False  # Disable saving logging artifacts
 
-    wandb_logger = WandbLogger(name='learn2D-jt', project='MSPE-rebuttal',
+    wandb_logger = WandbLogger(name='MSPE-vit-jt', project='MSPE-rebuttal',
                                entity='pigpeppa', offline=False)
-    checkpoint_callback = ModelCheckpoint(dirpath='ckpt/MSPE-rebuttal/learn2D', save_last=True)
+    checkpoint_callback = ModelCheckpoint(dirpath='ckpt/MSPE-rebuttal/MSPE-vit-jt', save_last=True)
 
     trainer = pl.Trainer.from_argparse_args(args, logger=wandb_logger, callbacks=[checkpoint_callback])
     for image_size, patch_size in [(224, 16)]:
