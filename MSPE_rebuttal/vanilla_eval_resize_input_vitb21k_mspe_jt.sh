b1bddc04c5ad3eca227ded9cfee6c96cbb9fb267
lwz <905328134@qq.com>
2024-08-04 20:05:25 +0800

    initial

--- MSPE_rebuttal/vanilla_eval_resize_input_vitb21k_mspe_jt.sh
+++ MSPE_rebuttal/vanilla_eval_resize_input_vitb21k_mspe_jt.sh
@@ -3,7 +3,7 @@ python vanilla_eval_resize_weight_vitb21k_rebuttal_mspe_jt.py \
   --accelerator gpu \
   --works 8 \
   --precision 16 \
-  --batch_size 64 \
+  --batch_size 32 \
   --log_every_n_steps 1 \
   --root /ppio_net0/torch_ds/imagenet \
   --model.weights vit_base_patch16_224.augreg2_in21k_ft_in1k \
